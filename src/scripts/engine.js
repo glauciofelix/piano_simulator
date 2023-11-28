f12444b721178bcf075960f67de939c7c0f3e584
Glaucio Felix <glaucio.felix@casaspedro.local>
2023-11-27 21:18:02 -0300

    :sparkles: feat: Implementado JavaScript

---
+++ src/scripts/engine.js
@@ -0,0 +1,30 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let mapedKeys = [];
+
+let audio = new Audio("src/tunes/a.wav");
+
+const playtune = (key) => {
+	audio.src = `src/tunes/${key}.wav`;
+	audio.play();
+
+	const clickedKey = document.querySelector(`[data-key="${key}"]`);
+	clickedKey.classList.add("active");
+	setTimeout(() => {
+		clickedKey.classList.remove("active");
+	}, 150);
+};
+
+
+pianoKeys.forEach((key) => {
+	console.log(key.dataset.key);
+
+	key.addEventListener("click", () => playtune(key.dataset.key));
+	mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+	if (mapedKeys.includes(e.key)) {
+		playtune(e.key);
+	};
+});
